43689088e2705b6685ef5ff3f3a158548393fd50
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-08-29 20:01:25 +0300

    add HomePage

---
+++ src/components/pages/TruckPage/TruckPage.jsx
@@ -0,0 +1,9 @@
+{
+  /* <picture>
+          <source srcSet={backgroundImgWebp} type="image/webp" />
+          <source srcSet={backgroundImgPng} type="image/png" />
+          <img src={backgroundImgPng} alt="Truck on the nature with sunset" />
+        </picture> */
+}
+// import backgroundImgWebp from "/src/components/images/HomePage/background.webp";
+// import backgroundImgPng from "/src/components/images/HomePage/background.png";
