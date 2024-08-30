ec37a4baafb9450057feadfca645f2718dea399e
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-08-30 14:12:13 +0300

    Update Filters.jsx

--- src/components/SideBar/Filters/Filters.jsx
+++ src/components/SideBar/Filters/Filters.jsx
@@ -12,7 +12,7 @@ const Filters = () => {
           <div className={css.containerDescription}>
             <svg width={32} height={32}>
               <use
-                href={`/src/assets/icons/icons.svg#icon-ac`}
+                xlinkHref={`/src/assets/icons/icons.svg#icon-ac`}
               ></use>
             </svg>
             <p className={css.filterDescription}>AC</p>
